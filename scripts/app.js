3a680f933955e8058f4821d8508deb71e38ce15a
Manuel Tiburcio <manuelmtiburcio@gmail.com>
2021-12-14 19:01:44 -0400

    Added F and country name

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,9 @@ const card = document.querySelector('.card');
 const details = document.querySelector('.details');
 const time = document.querySelector('img.time');
 const icon = document.querySelector('.icon img');
+const body = document.querySelector('body');
+const h1 = document.querySelector('h1');
+const form = document.querySelector('form');
 
 const updateUI = (data) => {
 
@@ -14,24 +17,34 @@ const updateUI = (data) => {
     // update details template
     details.innerHTML = `
     <h5 class="my-3">${cityDetails.EnglishName}</h5>
+    <h6 class="my-3">${cityDetails.Country.LocalizedName}</h6>
     <div class="my-3">${weather.WeatherText}</div>
     <div class="display-4 my-4">
         <span>${weather.Temperature.Metric.Value}</span>
         <span>&deg;C</span>
+        <hr>
+        <span>${weather.Temperature.Imperial.Value}</span>
+        <span>&deg;F</span>
     </div>
     `;
 
 
     // update the night/dat & icon images
-    const iconSrc = 'img/icons/${weather.WeatherIcon}.svg';
+    const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
     icon.setAttribute('src', iconSrc);
 
     let timeSrc = null;
 
     if(weather.IsDayTime){
         timeSrc = 'img/day.svg';
+        body.style.backgroundImage = "linear-gradient(to top, #cfd9df 0%, #e2ebf0 100%)";
+        h1.classList.replace('text-light', 'text-muted');
+        form.classList.replace('text-light', 'text-muted');
     } else {
-        timeSrc = 'image/night.svg';
+        timeSrc = 'img/night.svg';
+        body.style.backgroundImage = "linear-gradient(to top, #bac8e0 0%, #6a85b6 100%)";
+        h1.classList.replace('text-muted', 'text-light');
+        form.classList.replace('text-muted', 'text-light');
     }
 
     time.setAttribute('src', timeSrc);
